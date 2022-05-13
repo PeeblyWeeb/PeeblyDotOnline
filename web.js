4ef8f03e9605655bcf44e70f7357cc934de80804
5qc <roc0ast3r@gmail.com>
2022-05-12 19:35:18 -0700

    thefunny change

--- web.js
+++ web.js
@@ -1,34 +1,34 @@
-console.warn("Hey! get out of here! I don't want you seeing my terrible code!");
+console.warn("Hey! get out of here! I don't want you seeing my terrible code!")
 console.warn("also dont type 'thefunny()' in this console window")
 
 let thefunnycount = 0
 
-function thefunny()
-{   
+const thefunny = () => {   
     switch(thefunnycount) {
         case 0:
             console.log("I told you nothings here, just close devtools and go away. dont type thefunny again. just go")
             thefunnycount = thefunnycount + 1
-            break;
+            break
         case 1:
             console.log("Come on, i told you to leave!")
             thefunnycount = thefunnycount + 1
-            break;
+            break
         case 2:
             console.log("Please, just leave.")
             thefunnycount = thefunnycount + 1
-            break;
+            break
         case 3:
             console.warn("GOD DAMNIT, FUCKING LEAVE!")
             thefunnycount = thefunnycount + 1
-            break;
+            break
         case 4:
             console.warn("Fine, do it again. i dare you.")
             thefunnycount = thefunnycount + 1
-            break;
+            break
         case 5:
-            window.location.replace("https://google.com");
-            break;
+            let a = 0
+            for (let i = 0; i < 1; a++) console.error("GOODBYE")
+            break
     }    
 }
 
